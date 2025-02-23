9c6146c8c7f11a29340190e552791a785ba355c6
nastia <nasti@COMPNASTIA>
2025-02-23 21:05:11 +0200

    change design for service creation and editing

--- PetCareApp/PetCareApp/Dtos/AddServiceDto.cs
+++ PetCareApp/PetCareApp/Dtos/AddServiceDto.cs
@@ -4,26 +4,37 @@ namespace PetCareApp.Dtos
 {
     public class AddServiceDto
     {
-        [Required]
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
-        [Required]
-        public string Description { get; set; } 
-        [Range(0, 100000)]
-        [Required]
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Minimum price is required.")]
+        [Range(0, 100000, ErrorMessage = "Minimum price must be between 0 and 100,000.")]
         public float MinimumPrice { get; set; }
-        [Range(0, 100000)]
-        [Required]
+
+        [Required(ErrorMessage = "Maximum price is required.")]
+        [Range(1, 100000, ErrorMessage = "Maximum price must be between 0 and 100,000.")]
         public float MaximumPrice { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Real time is required.")]
+        [Range(1, 1000, ErrorMessage = "Real time must be between 1 and 1000 minutes.")]
         public int RealTime { get; set; }
+
+        [Required(ErrorMessage = "Minimum time is required.")]
+        [Range(1, 1000, ErrorMessage = "Minimum time must be between 1 and 1000 minutes.")]
+        public int MinimumTime { get; set; }
+
+        [Required(ErrorMessage = "Maximum time is required.")]
+        [Range(1, 1000, ErrorMessage = "Maximum time must be between 1 and 1000 minutes.")]
+        public int MaximumTime { get; set; }
         [Required]
         public bool IsFixedTime { get; set; }
         [Required]
         public bool IsNearestTime { get; set; } = false;
         [Required]
-        public int MinimumTime { get; set; }
-        [Required]
-        public int MaximumTime { get; set; }
+        
         public bool IsHidden { get; set; } = false;
     }
 }
